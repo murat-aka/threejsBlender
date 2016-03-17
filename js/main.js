feea3c0ed79d6d2550b18a34188354fb114d77ec
murat aka <murat-aka@hotmail.com>
2016-03-17 16:44:48 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ function initCamera() {
          camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
      // position and point the camera to the center of the scene
         camera.position.x = -30;
-        camera.position.y = 40;
+        camera.position.y = 20;
         camera.position.z = 30;
         camera.lookAt(scene.position);
     
